998fa3ec033163d3f00256d30d0caf58bf4c66df
shylot <remixjc@foxmail.com>
2021-04-28 11:34:03 +0800

    增加1153信息 修正openid

--- info.js
+++ info.js
@@ -10,3 +10,16 @@ function _1068(){
     }
     return info;
 }
+
+function _1153(){
+    var info={
+        data: {
+            "company": "wanji",
+            "username": "1153",
+            "password": "dhm19980221",
+            "source": 5,
+            "openId": "oq6T94qr-CC1BEvD4N54QTey0kw4"
+        }
+    }
+    return info;
+}
