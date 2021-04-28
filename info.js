90f8fa78849aab3f35b2c3ca5e1e1472e39821fc
shylot <remixjc@foxmail.com>
2021-04-28 11:25:34 +0800

    优化登录方案

---
+++ info.js
@@ -0,0 +1,12 @@
+function _1068(){
+    var info={
+        data: {
+            "company": "wanji",
+            "username": "1068",
+            "password": "Wc901230",
+            "source": 5,
+            "openId": "oq6T94vWe8rwVODEvaNupHkGnE9s"
+        }
+    }
+    return info;
+}
