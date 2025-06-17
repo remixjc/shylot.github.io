dc72af21973a9c0ed58fda973a8a3a5a8c9cc44a
Hack Jacob <539526193@qq.com>
2025-06-17 18:52:50 +0800

    Update info.js

--- info.js
+++ info.js
@@ -15,10 +15,10 @@ function _1153(){
     var info={
         data: {
             "company": "wanji",
-            "username": "1153",
-            "password": "dhm19980221",
+            "username": "1333",
+            "password": "1a2b0c3d",
             "source": 5,
-            "openId": "oq6T94qr-CC1BEvD4N54QTey0kw4"
+            "openId": "oq6T94rD9ioEMcPQu8Vh0Ac7d4U0"
         }
     }
     return info;
@@ -28,10 +28,10 @@ function _1260(){
     var info={
         data: {
             "company": "wanji",
-            "username": "1260",
-            "password": "063600Cgh",
+            "username": "1264",
+            "password": "a568192195",
             "source": 5,
-            "openId": "oq6T94hSwA_DPsYN96yLVK_XLH_I"
+            "openId": "oq6T94mC11UqLNawJ4AoIe2n2gAg"
         }
     }
     return info;
